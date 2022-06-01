ac264994985c58d94a3ab708abd76d7b4dad37f6
xiao <im.qxxiao@gmail.com>
2022-06-01 21:01:48 +0800

    update

--- 面试/1. 蛇形矩阵.cc
+++ 面试/1. 蛇形矩阵.cc
@@ -7,28 +7,24 @@ using namespace std;
 int m, n;
 int res[100][100];
 
-int main()
-{
+int main() {
     cin >> n >> m;
 
     int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
 
-    for (int x = 0, y = 0, k = 1, d = 0; k <= m * n; k++)
-    {
-        res[x][y] = k; // 当前是可行的位置
+    for (int x = 0, y = 0, k = 1, d = 0; k <= m * n; k++) {
+        res[x][y] = k;  // 当前是可行的位置
         int a = x + dx[d], b = y + dy[d];
-        if (a < 0 || a >= n || b < 0 || b >= m || res[a][b])
-        { // 下一个可行的位置
+        if (a < 0 || a >= n || b < 0 || b >= m || res[a][b]) {  // 下一个可行的位置
             d = (d + 1) % 4;
             a = x + dx[d], b = y + dy[d];
         }
         x = a, y = b;
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-            cout << res[i][j] << " ";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) cout << res[i][j] << " ";
         cout << endl;
     }
+    return 0;
 }
